4011e0d999596e95f3eace433515a6a7170c9821
duracell80 <lee.jordan@gmail.com>
2019-11-29 16:08:24 -0600

    Change stream from 140 to 249 to try to get a full length stream

--- scripts/make-yt/playlist_load.sh
+++ scripts/make-yt/playlist_load.sh
@@ -7,7 +7,7 @@
 # #EXTINF:-1, YouTube Audio
 # http://localhost/yt-play/?src=6LEmDEZVa5g ...
 
-sudo youtube-dl -f 140 -j $1 > /var/www/yt-play/source.json
+sudo youtube-dl -f 249 -j $1 > /var/www/yt-play/source.json
 url=$(sudo python source_url.py)
 title=$(sudo python source_title.py)
 
@@ -15,4 +15,4 @@ sudo echo "#EXTINF:-1, YouTube: "${title} >> /var/www/yt-play/yt.m3u
 sudo echo ${url} >> /var/www/yt-play/yt.m3u
 sudo cp -f /var/www/yt-play/yt.m3u /var/lib/mpd/playlists/YouTube_Play.m3u
 
-echo "I finish up my coffee and it's time to catch the train -- Suzanne Vega" > /var/www/yt-play/source.json
+# echo "I finish up my coffee and it's time to catch the train -- Suzanne Vega" > /var/www/yt-play/source.json
