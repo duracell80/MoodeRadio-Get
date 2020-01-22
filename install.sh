43b6c87a780d6b4ad08a33793e6982863c94c28d
duracell80 <lee.jordan@gmail.com>
2020-01-21 22:15:47 -0600

    .

--- install.sh
+++ install.sh
@@ -11,3 +11,5 @@ sudo cp -r ./packs/stations/ /mnt/SDCARD/_Stations
 
 current_dir=$PWD;cd /mnt/SDCARD/_Stations/stations/us/tn/bna/;sudo python import.py;cd $current_dir;
 current_dir=$PWD;cd /mnt/SDCARD/_Stations/stations/uk/eng/lhr/;sudo python import.py;cd $current_dir;
+
+# other tasks
