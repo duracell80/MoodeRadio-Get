a9b44946008c22f8291cd6fc08efd3fdd41ab208
duracell80 <lee.jordan@gmail.com>
2020-02-23 20:08:01 -0600

    Install Tweaks

--- scripts/make-radio-manager/install.sh
+++ scripts/make-radio-manager/install.sh
@@ -38,8 +38,8 @@ sudo chmod 777 /var/www/radio/sources/moode/user/radio-logos/thumbs
 sudo chmod 777 /var/www/radio/sources/config.json
 sudo chmod 777 /var/www/radio/sources/rb/tags.json
 sudo chmod 755 /var/www/radio/sources/rb/*.py
-sudo chmod 755 /var/www/radio/sources/moode/*.py
+sudo chmod 755 /var/www/radio/sources/moode/scripts/*.py
 sudo chmod 755 /var/www/rdo-config.php
 sudo chmod 755 /var/www/rdo-config-rb.php
 sudo chmod 755 /var/www/templates/rdo-config.html
-sudo chmod 755 /var/www/templates/rdo-config-usr.html
+sudo chmod 755 /var/www/templates/rdo-config-rb.html
