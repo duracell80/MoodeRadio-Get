45ac4155f20f3500005f74ab79b2c983e9d294ea
duracell80 <lee.jordan@gmail.com>
2020-04-27 23:17:44 -0500

    Adding Podcast Refresh API

---
+++ scripts/make-radio-manager/radio/sources/pod/index.php
@@ -0,0 +1,177 @@
+<?php 
+/** Podcast-Play API
+  *
+  * Lee Jordan @duracell80
+  * 04/27/2020
+
+    
+
+*/
+
+
+// Main Params
+$cmd        = $_GET["cmd"];
+$src        = $_GET["src"];
+$type       = $_GET["type"];
+
+// Podcast Params
+if(isset($_GET["name"]) && !empty($_GET["name"])){ $name = $_GET["name"]; } else { $name = "generic";}
+if(isset($_GET["items"]) && !empty($_GET["items"])){ $items = $_GET["items"]; } else { $items = "5";}
+if(isset($_GET["interlude"]) && !empty($_GET["interlude"])){ $interlude = $_GET["interlude"]; } else { $interlude = "0";}
+
+
+
+$apiPath    = "/var/www/radio/sources/pod";
+
+
+    switch ($type) {      
+    case "podcast":
+        if($interlude == "1"){
+            // SEND an MP3 Header to Moode rather than a PHP file
+            $interlude = "/var/www/radio/sources/pod/interlude.mp3";
+            if(file_exists($interlude)) {
+                header('Content-Type: audio/mpeg');
+                header('Content-Disposition: filename="interlude.mp3"');
+                header('Content-length: '.filesize($interlude));
+                header('Cache-Control: no-cache');
+                header("Content-Transfer-Encoding: chunked"); 
+
+                readfile($interlude);
+                
+                $runcmd = "python " . $apiPath . "/pod2m3u.py " . $src . " " . $name . " " . $items;
+                shell_exec($runcmd);
+                
+            } else {
+                header("HTTP/1.0 404 Not Found");
+            }
+        } else {
+            // EXAMPLE http://192.168.2.4/radio/sources/pod?type=podcast&src=https://www.spreaker.com/show/3287246/episodes/feed&items=5&name=skynews
+            $runcmd = "python " . $apiPath . "/pod2m3u.py " . $src . " " . $name . " " . $items;
+            echo(shell_exec($runcmd));
+            header("Location: /");
+        }
+        
+        break;        
+    
+    
+    case "moode":
+        
+        
+    case "mpc":
+        if(isset($cmd) && !empty($cmd)){
+            switch ($cmd) {
+
+                // UPDATE DATABASE
+                case "update":
+                    $runcmd = "mpc update";
+                    echo(shell_exec($runcmd));
+                    break;
+
+                // STATUS
+                case "status":
+                    $runcmd = "mpc status";
+                    echo(shell_exec($runcmd));
+                    break;
+
+                // LIST Playlists
+                case "list":
+                    $runcmd = "mpc lsplaylists";
+                    $list   = shell_exec($runcmd); 
+                    $playlists = explode("\n", $list);
+
+                    header('Content-type: text/javascript');
+                    echo(pretty_json(json_encode($playlists)."\n"));
+
+                    break;
+
+                // STOP
+                case "stop":
+                    $runcmd = "mpc stop";
+                    echo(shell_exec($runcmd));
+                    break;
+
+                // PLAY
+                case "play":
+                    $runcmd = "mpc play";
+                    echo(shell_exec($runcmd));
+                    break;
+
+                // PAUSE
+                case "pause":
+                    $runcmd = "mpc pause-if-playing";
+                    echo(shell_exec($runcmd));
+                    break;
+
+                // PREV
+                case "prev":
+                    $runcmd = "mpc prev";
+                    echo(shell_exec($runcmd));
+                    break;
+
+                // NEXT
+                case "next":
+                    $runcmd = "mpc next";
+                    echo(shell_exec($runcmd));
+                    break;
+
+                // SKIP FORWARD 15s
+                case "fwd15":
+                    $runcmd = "mpc seek +15";
+                    echo(shell_exec($runcmd));
+                    break;
+
+                // SKIP FORWARD 30s
+                case "fwd30":
+                    $runcmd = "mpc seek +30";
+                    echo(shell_exec($runcmd));
+                    break;
+
+                // SKIP FORWARD 60s
+                case "fwd60":
+                    $runcmd = "mpc seek +60";
+                    echo(shell_exec($runcmd));
+                    break;
+
+                // SKIP FORWARD 5m
+                case "fwd5m":
+                    $runcmd = "mpc seek +300";
+                    echo(shell_exec($runcmd));
+                    break;
+
+                // SKIP BACK 15s
+                case "bck15":
+                    $runcmd = "mpc seek -15";
+                    echo(shell_exec($runcmd));
+                    break;
+
+                // SKIP BACK 30s
+                case "bck30":
+                    $runcmd = "mpc seek -30";
+                    echo(shell_exec($runcmd));
+                    break;
+
+                // SKIP BACK 60s
+                case "bck60":
+                    $runcmd = "mpc seek -60";
+                    echo(shell_exec($runcmd));
+                    break;
+
+                // SKIP BACK 5m
+                case "bck5m":
+                    $runcmd = "mpc seek -300";
+                    echo(shell_exec($runcmd));
+                    break;
+
+
+
+
+                default:
+                    break;
+            }
+        }
+        break;
+        
+    default:
+    break;
+}
+?>
--- scripts/make-radio-manager/radio/sources/pod/pod2m3u.py
+++ scripts/make-radio-manager/radio/sources/pod/pod2m3u.py
@@ -5,25 +5,22 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 
 
-# Get feed of podcast else set default
+# GET feed of podcast else set default
 try:
-    print("Podcast Feed: " + sys.argv[1])
     feedurl = sys.argv[1]
 except IndexError as e:
     # Skynews Daily Podcast by Default
     feedurl = "https://www.spreaker.com/show/3287246/episodes/feed"
     
-# Get name of podcast else set default
+# GET name of podcast else set default
 try:
-    print("Podcast Playlist Name: podcast_" + sys.argv[2])
     pod_name = sys.argv[2]
 except IndexError as e:
     pod_name = "default"    
 
 
-# Get name of podcast else set default
+# GET name of podcast else set default
 try:
-    print("Podcast Episodes: " + sys.argv[3])
     pod_items = sys.argv[3]
 except IndexError as e:
     pod_items = 5
@@ -33,7 +30,7 @@ except IndexError as e:
 
     
     
-    
+# GET the dictonary object from Podcastparser    
 data            = podcastparser.parse(feedurl, urllib.urlopen(feedurl), int(pod_items))
 pod_title       = data["title"]
 pod_timeformat  = "%m/%d/%Y"
@@ -60,40 +57,49 @@ pod_m3u         = "#EXTM3U\n"
 # episode_art_url
 
 
-
+# CHECK for cover art
 if "http" in data["cover_url"]:
     pod_art = data["cover_url"]
-    print("Podcast Cover Art: " + pod_art)
 
     
-
+# PARSE each episode
 for s in data["episodes"]:
-    
-        
     pod_date    = datetime.fromtimestamp(s["published"])
     pod_m3u     = pod_m3u + "#EXTINF:"+ str(s["total_time"])+ "," + pod_title + " - " + pod_date.strftime(pod_timeformat) + " - " + s["title"] + "\n"
     
     for e in s["enclosures"]:
         pod_m3u = pod_m3u + e["url"] + "\n\n"
 
-# SET File        
+        
+        
+        
+        
+        
+        
+# WRITE last item to call back to API to check for more episodes
+pod_m3u = pod_m3u + "#EXTINF:-1,"+ pod_title + " - Check For Latest "+pod_items+" Episodes\n"
+pod_m3u = pod_m3u + "http://localhost:80/radio/sources/pod/?type=podcast&src="+feedurl+"&name="+pod_name+"&items="+pod_items+"&interlude=1"
+        
+# SET file
+os.system("sudo mkdir -p /var/lib/mpd/music/RADIO/_Podcasts")
 os.system("sudo touch podcast_"+pod_name+".m3u")
 os.system("sudo chmod 777 podcast_"+pod_name+".m3u")
 with open("podcast_"+pod_name+".m3u", "w") as f_m3u:
     f_m3u.write(pod_m3u)
     
-# GET Cover Art
+# GET cover art
 r = requests.get(pod_art)
 os.system("sudo touch /var/www/images/radio-logos/podcast_"+pod_name+".jpg")
 os.system("sudo chmod 777 /var/www/images/radio-logos/podcast_"+pod_name+".jpg")
 with open("/var/www/images/radio-logos/podcast_"+pod_name+".jpg", "wb") as f:
-    f.write(r.content)    
+    f.write(r.content)       
 
-os.system("sudo cp -f /var/www/images/radio-logos/podcast_"+pod_name+".jpg /var/www/images/radio-logos/thumbs/")    
-os.system("sudo mkdir -p /var/lib/mpd/music/RADIO/_Podcasts")
 
-# MOVE File
+# COPY files
+os.system("sudo cp -f /var/www/images/radio-logos/podcast_"+pod_name+".jpg /var/www/images/radio-logos/thumbs/") 
 os.system("sudo cp -f *.m3u /var/lib/mpd/playlists/")
 os.system("sudo cp -f *.m3u /var/lib/mpd/music/RADIO/_Podcasts/")
+
+# UPDATE MPD, clear, load and play
 os.system("mpc update")
-os.system("mpc clear; mpc load podcast_"+pod_name+"; mpc play")
+os.system("mpc stop; mpc clear; mpc load podcast_"+pod_name+"; mpc play")
